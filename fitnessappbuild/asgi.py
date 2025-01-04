d336af7a274ccc4261c291d7dc2194840ad64929
James Kuligowski <jdkuligowski@gmail.com>
2025-01-04 14:38:50 +0000

    changing build number

--- fitnessappbuild/asgi.py
+++ fitnessappbuild/asgi.py
@@ -8,19 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chat_room.routing import websocket_urlpatterns  # Import your WebSocket routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitnessappbuild.settings")
 
-# Set up ASGI application to handle HTTP and WebSocket protocols
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Handle traditional HTTP requests
-    "websocket": AuthMiddlewareStack(  # Handle WebSocket connections
-        URLRouter(
-            websocket_urlpatterns  # Include WebSocket URL patterns
-        )
-    ),
-})
+application = get_asgi_application()
--- fitnessappbuild/settings.py
+++ fitnessappbuild/settings.py
@@ -184,14 +184,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ASGI_APPLICATION = "fitnessappbuild.asgi.application"
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],  # Redis URL
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)],  # Redis URL
+#         },
+#     },
+# }
 
 LOGGING = {
     'version': 1,
