cf5c99f8874384ea70fd42d6c294a7f6bcdbf9da
James Kuligowski <jdkuligowski@gmail.com>
2025-03-17 14:56:30 +0000

    add beat worker

--- startup.sh
+++ startup.sh
@@ -3,8 +3,11 @@
 # Optionally install dependencies
 pip install --no-cache-dir -r requirements.txt
 
-# Start a minimal HTTP server in the background
+# Start a minimal HTTP server in the background on port 8000
 python -m http.server 8000 &
 
-# Start Celery in the foreground
-celery -A fitnessappbuild worker -l info
+# Start Celery worker in the background
+celery -A fitnessappbuild worker -l info &
+
+# Finally, start Celery Beat in the foreground
+celery -A fitnessappbuild beat -l info
